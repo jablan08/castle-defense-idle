d543bebb0a97ad83003e78bcee87f6a87f6a9a5b
Jablan <joshuaablan@gmail.com>
2019-04-05 18:47:43 -0700

    add enemy movement and collision interaction between player

--- js/app.js
+++ js/app.js
@@ -7,15 +7,13 @@ const mouse = {
     y: innerHeight / 2
 }
 
-
-function collision(x1, width1, x2, width2) {
-    if (x1 < x2 + width2-80&& x1 + width1 > x2){
-        return true;
-    } else {
-        return false;
-    }
-}
-function getDistance(x1, y1, x2, y2){
+let hit = false;
+// function collision() {
+//     else {
+//         return false;
+//     }s
+// } 
+function getDistance(x1, y1, x2, y2){ 
     let xDistance = x2 - x1;
     let yDistance = y2 - y1;
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
@@ -25,7 +23,7 @@ function getDistance(x1, y1, x2, y2){
 addEventListener("mousemove", function(event){
     mouse.x = event.clientX;
     mouse.y = event.clientY;
-    console.log(event)
+    // console.log(event)
 })
 
 addEventListener('resize', () => {
@@ -54,7 +52,9 @@ function Keyboarder() {
     this.KEYS = { 
         LEFT: 37,
         RIGHT: 39,
-        SPACE: 32
+        SPACE: 32,
+        UP: 38,
+        DOWN: 40
     };
 };
 
@@ -79,6 +79,7 @@ function Player(name) {
     this.name = name;
     this.x = 300;
     this.y = 300;
+    this.attackValue = 2;
     // this.dx = 0;
     // this.dy = 0;
     // this.radius = 40;
@@ -107,31 +108,45 @@ function Player(name) {
         
         if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
             // console.log("left key press")
+            image.src = "imgs/war.spread.png"
             this.x -=4 ;
         } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+            image.src = "imgs/war.spread.right.png"
             this.x += 4;
         }  else if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
             srcY = attackPos * this.frameHeight;
             this.attack();
-        } else {
+        } /*else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP)) {
+            
+            this.y -= 4;
+        } else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN)) {
+
+            this.y += 4;
+        } */else {
             srcY = idlePos * this.frameHeight;
         }
         this.draw();
 
     }
     this.attack = function() {
-        if (collision(this.x,this.frameWidth,enemey.x,enemey.frameWidth)===true) {
+        if (player1.x < enemy.x + enemy.frameWidth-100&&
+            player1.x + player1.frameWidth-50> enemy.x){
             console.log("has hit!")
+            enemy.hp -= this.attackValue;
+            hit = true;
+        } else {
+            
         }
     }
 
 }
 
-
+const imgs = ["imgs/enemy.png", "imgs/enemy.flip.png"];
 function Enemy() {
     let srcX; 
     let srcY;
     let attackPos = 1;
+    let getHit = 2;
     let idlePos = 0;
     let sheetWidth = 1240;
     let sheetHeight = 453;
@@ -141,9 +156,9 @@ function Enemy() {
     this.frameWidth = sheetWidth / cols;
     this.frameHeight = sheetHeight / rows;
     let currentFrame = 0;
-    const image = new Image();
-    image.src ="imgs/enemy.png";
-
+    this.image = new Image();
+    this.image.src = imgs[0];
+    this.velocity = 10;
     this.name = name;
     this.x = 200;
     this.y = 250;
@@ -155,7 +170,7 @@ function Enemy() {
     this.moveSpeed = 2
     this.hp = Math.floor((Math.random())* 4)+1;
     this.draw = function() {
-        cxt.drawImage(image, srcX, srcY, this.frameWidth, this.frameHeight, this.x,this.y,this.frameWidth,this.frameHeight)
+        cxt.drawImage(this.image, srcX, srcY, this.frameWidth, this.frameHeight, this.x,this.y,this.frameWidth,this.frameHeight)
         // cxt.fillStyle = "rgba(255, 255, 0, 0.5)"
         // cxt.fillRect(this.x, this.y, this.width, this.height)
        
@@ -166,19 +181,41 @@ function Enemy() {
         // cxt.closePath
     }
     this.update = function() {
-        currentFrame = ++currentFrame % cols;
-        srcX = currentFrame * this.frameWidth;
-        srcY = idlePos * this.frameHeight;        
-        this.draw();
+        if (hit === true) {
+            console.log("enemy got hit")
+            // currentFrame = ++currentFrame % cols;
+            if (this.image.src === "file:///Users/mioji/seiLA/rpg_game/imgs/enemy.flip.png"){
+                console.log("worked!")
+                srcX = 1 * this.frameWidth;
+                srcY = getHit * this.frameHeight;
+                this.x += 24;
+            } else
+            srcX = 3 * this.frameWidth;
+            srcY = getHit * this.frameHeight;
+            this.x -= 12;
+        } else {
+            currentFrame = ++currentFrame % cols;
+            srcX = currentFrame * this.frameWidth;
+            srcY = idlePos * this.frameHeight; 
+        }
+        if (this.x + this.frameWidth >= innerWidth) {
+            this.velocity = -this.velocity;
+            this.image.src = imgs[1];
+        } else if(this.x <= 0 ) {
+            this.image.src = imgs[0];
+            this.velocity = -this.velocity;
+        }
+        this.x += this.velocity;  
+        this.draw();  
     }
 }
 
 // creating characters on the board
 let player1;
-let enemey;
+let enemy;
 function init() {
     player1 = new Player("Josh")
-    enemey = new Enemy()
+    enemy = new Enemy()
 
 }
 
@@ -187,18 +224,13 @@ function init() {
 let animate2 = setInterval(function(){
     cxt.clearRect(0,0, innerWidth,innerHeight);
     player1.update();
-    enemey.update();
+    enemy.update();
+    hit = false;
     
 }, 100)
-const animate = () => {
-
-    // requestAnimationFrame(animate);
-    cxt.clearRect(0,0, innerWidth,innerHeight);
-    player1.update();
-    enemey.update();
-}
+//  
 init();
-animate();
+// animate();
 
 // player class 
 // includes HP, defense, attack, maybe magic power, and special?
